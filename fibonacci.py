385b5b683e98bb004ec9f2254c63ac25654c30f8
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2019-02-07 17:16:17 -0600

    Fixed for all cases

--- fibonacci.py
+++ fibonacci.py
@@ -2,19 +2,20 @@ import numpy as np
 from numpy.linalg import inv
 
 def expBySquaring(x, n):
-	if n < 0:
-		return expBySquaring(inv(x), - n)
-	elif n == 0:
-		return 1
-	elif n == 1: 
+	if n == 1:
 		return x
-	elif n % 2 == 0: # Pair case
-		return expBySquaring(x.dot(x),  n / 2)
-	elif n % 2 == 1: # Odd case
-		return x.dot(expBySquaring(x.dot(x), (n - 1) / 2))
+	if n % 2 == 0:
+		return expBySquaring(x.dot(x), n // 2)
+	return x.dot(expBySquaring(x.dot(x), n // 2))
 
-n = input('n = ')
+n = int(input('n = '))
 m = np.array([[1, 1], [1, 0]], dtype = np.object)
-fib_m = expBySquaring(m, int(n) - 1)
+
+if n > 1:
+	fib_m = expBySquaring(m, n - 1)
+elif n == 1:
+	fib_m = np.array([[1, 0], [0, 0]], dtype = np.object)
+elif n == 0:
+	fib_m = np.array([[0, 0], [0, 0]], dtype = np.object)
 
 print(f'fibonacci({n}) = {fib_m[0, 0]}')
