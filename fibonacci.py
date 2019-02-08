87fbff4d24a934452b93a240cab522caf873f29d
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2019-02-07 23:11:52 -0600

    Import cleansing

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,11 @@
 import numpy as np
-from numpy.linalg import inv
 
 def expBySquaring(x, n):
-	if n == 1:
+	if n == 1: # Stop criteria
 		return x
-	if n % 2 == 0:
+	if n % 2 == 0: # N is even
 		return expBySquaring(x.dot(x), n // 2)
-	return x.dot(expBySquaring(x.dot(x), n // 2))
+	return x.dot(expBySquaring(x.dot(x), n // 2)) # N is odd
 
 n = int(input('n = '))
 m = np.array([[1, 1], [1, 0]], dtype = np.object)
@@ -14,8 +13,8 @@ m = np.array([[1, 1], [1, 0]], dtype = np.object)
 if n > 1:
 	fib_m = expBySquaring(m, n - 1)
 elif n == 1:
-	fib_m = np.array([[1, 0], [0, 0]], dtype = np.object)
+	fib_m = np.array([[1]], dtype = np.object)
 elif n == 0:
-	fib_m = np.array([[0, 0], [0, 0]], dtype = np.object)
+	fib_m = np.array([[0]], dtype = np.object)
 
 print(f'fibonacci({n}) = {fib_m[0, 0]}')
